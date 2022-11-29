dd166c0be3e8bfa7d3490bd952e1320e9655c31a
Md. Mubassir Ahsan <mahsandu@gmail.com>
2022-11-29 17:30:15 +0600

    list creation

---
+++ src/main/kotlin/class4.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val adminUser = getAdminUser()
+    println(adminUser)
+//    println(adminUsers)
+//    println()
+//    println(getAdminUser())
+//    println(getAdminUser())
+}
+val adminUsers: List<Int> = listOf(10,20,30)
+fun getAdminUser(): List<Int> {
+//    return listOf(100,600,900)
+    return adminUsers
+}
